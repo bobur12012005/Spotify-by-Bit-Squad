b6429c0b31688af5d3b2aab77896fa41f1eff520
Bobur Makhmadkulov <boburmakhmadkulov05@gmail.com>
2024-08-19 14:53:31 +0500

    comment

--- src/components/Followings.jsx
+++ src/components/Followings.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 function Followings({ item }) {
+     const imageUrl = item.track.album.images[0]?.url
     return (
         <a href="#" className="link">
             <div className="following-singer">
                 <div className="img-side">
-                    <img src={item.image} />
+                    <img src={imageUrl} />
                 </div>
                 <div className="name-side">
-                    <span>{item.name}</span>
+                    <span>{item.track.name}</span>
                     <button>
                         <img src="/icons/play.svg" alt="Play" />
                     </button>
